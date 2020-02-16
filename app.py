1d1168009b16f063cc2640036c061d037c0e8f01
hmothera <hmothera@umich.edu>
2020-02-15 19:38:55 -0600

    Trying mongoengine db

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 from flask import Flask, url_for, request, render_template, session, redirect
 from markupsafe import escape
-from flask_mongoengine import MongoEngine
-from models import *
+from flask_mongoengine import MongoEngine, MongoEngineSessionInterface
+from flask_mongoengine.wtf import model_form
+
 
 # creates the Flask application
 app = Flask(__name__)
-app.config.from_pyfile('the-config.cfg')
 db = MongoEngine(app)
+app.session_interface = MongoEngineSessionInterface(db)
+
+
+class User(db.Document):
+    username = db.StringField(max_length=50)
+    password = db.StringField(max_length=50)
 
 # views:
 @app.route('/')
 def index():
 	return render_template("index.html")
+
+loginForm = model_form(User, field_args={'username': {'textarea': True}, 'password': {'password': True}})
 
 # GET is default method, just renders the login page
 # POST method is for users to send a POST request w their login info to /login
@@ -19,12 +27,15 @@ def index():
 def login():
 	error = None
 	if request.method == 'POST':
-		session['username'] = request.form['username']
+		#session['username'] = request.form['username']
+		current_user = User(username=request.form['username'], password=request.form['password'])
+		#add_user(request.form['username'], request.form['password'])
 		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
 			error = 'Invalid, please try again'
 		else:
 			return redirect('/')
 	return render_template("login.html", error=error)
+
 
 # def login():
 # error = None
@@ -50,6 +61,9 @@ def logout():
 def profile():
 	return render_template("profile.html")
 
+@app.route('/debugging')
+def view_output():
+	return User.objects.get
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -65,3 +79,12 @@ app.config["DEBUG"] = True
 
 # Secret key for sessions
 app.config["SECRET_KEY"] = b'H\xcf\xf9\x8f\x98\xf8\xe3\x82\xcfd'
+
+# mongoengine
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'project1',
+    'host': '192.168.1.35',
+    'port': 12345
+}
+
+
--- models.py
+++ models.py
@@ -1,25 +1,21 @@
+from app import app
 from flask_mongoengine.wtf import model_form
 
 class User(db.Document):
-    email = db.StringField(required=True)
-    first_name = db.StringField(max_length=50)
-    last_name = db.StringField(max_length=50)
+    username = db.StringField(max_length=50)
+    password = db.StringField(max_length=50)
 
-class Content(db.EmbeddedDocument):
-    text = db.StringField()
-    lang = db.StringField(max_length=3)
+# class Meme(db.Document):
+#     text = db.StringField()
+#     lang = db.StringField(max_length=3)
+#     tags = db.ListField(db.StringField(max_length=30))
+#     content = db.EmbeddedDocumentField(Content)
 
-class Post(db.Document):
-    title = db.StringField(max_length=120, required=True, validators=[validators.InputRequired(message=u'Missing title.'),])
-    author = db.ReferenceField(User)
-    tags = db.ListField(db.StringField(max_length=30))
-    content = db.EmbeddedDocumentField(Content)
+# PostForm = model_form(Post)
 
-PostForm = model_form(Post)
-
-def add_post(request):
-    form = PostForm(request.POST)
-    if request.method == 'POST' and form.validate():
-        # do something
-        redirect('done')
-    return render_template('add_post.html', form=form)
+# def add_post(request):
+#     form = PostForm(request.POST)
+#     if request.method == 'POST' and form.validate():
+#         # do something
+#         redirect('done')
+#     return render_template('add_post.html', form=form)
