b158996ea6e518d7d78e29ed284d1e43579c9a27
luketheshack <44590978+luketheshack@users.noreply.github.com>
2020-02-15 19:40:52 -0600

    Update freqPlot.py

--- frequency plotter/freqPlot.py
+++ frequency plotter/freqPlot.py
@@ -1,14 +1,31 @@
 import matplotlib.pyplot as plt
+import pymongo as pm
 
-data = open("memeData.txt", 'r')
+connection = pm.MongoClient('mongodb+srv://lukemarushack:gHmKkInrZNQeI3B6@economeme-0ffr6.mongodb.net/test?retryWrites=true&w=majority', 27017)
+db = connection.Economeme
+
+data = db.memes
+friendsList = data.find()
+
+fullSet = {}
+for item in friendsList:
+    fullSet[item['Meme']] = item['Freq']
+
+def returnData():
+    return fullSet
 
 def generatePlot(meme):
-    plotted = data[meme]
+    plotted = returnData()[meme]
     pastSevenVals = plotted[-7:-1]
     pastWeek = range(1,7)
-    plt.plot(pastWeek, pastSevenVals)
+    plt.plot(pastWeek, pastSevenVals, color=(127/255,74/255, 1,1))
     plt.xlabel('Current Trend')
     plt.ylabel('Estimated Impressions')
-    plt.title(meme + ' frequency over the past week')
+    plt.title('Frequency of the "' + meme + '" meme over the past week')
+    plt.show()
 
-generatePlot('Doge')
+imagesDict = {}
+
+for keyName in fullSet:
+    imagesDict[keyName] = generatePlot(keyName)
+
