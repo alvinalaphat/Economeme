5c65f0c7b0f1af5cc4ff38bd0f59624c71c36167
luketheshack <44590978+luketheshack@users.noreply.github.com>
2020-02-15 22:54:15 -0600

    Create clout.py

---
+++ math modeling/clout.py
@@ -0,0 +1,11 @@
+# defines the clout for an average individual. 
+# use a similar thing to the elo ranking system (points based on performace against high skill)
+# except this will be points for taking big guesses
+# users get lots of clout for guessing using large amounts of coins and winning
+# and do not lose much clout when guessing big and losing
+# players lose clout for playing small and losing, and do not gain much clout if play small and win
+
+
+initialClout = 0
+
+def userBet(coins, percentGuess, )
